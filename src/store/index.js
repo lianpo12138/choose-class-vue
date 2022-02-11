ac26f393836a1443f07541bc45ea38c9c056194f
wulinfeng <420564393@qq.com>
2022-02-11 16:04:55 +0800

    将id存入sessionstorage中

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import persistedState from 'vuex-persistedstate'
 Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
+    id: '',
     user: 'student',
     username: 'user',
     token:''
@@ -12,6 +13,7 @@ export default new Vuex.Store({
   mutations: {
     // 登录后将角色和用户名存入vuex，然后存入sessionstorage
     set (state, payload) {
+      state.id = payload.id;
       state.user = payload.role;
       state.username = payload.realname;
       state.token = payload.token;
